6d3f328c359a6559952a9459ceb9b5d78f987478
CVillemus <corentin.villemus@gmail.com>
2020-09-28 11:00:00 +0200

    first commit working maybe

---
+++ cdyshop/src/test/java/candyshop/cdy/CdyApplicationTests.java
@@ -0,0 +1,13 @@
+package candyshop.cdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
